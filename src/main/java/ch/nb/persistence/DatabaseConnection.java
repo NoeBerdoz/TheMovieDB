6646f7a404b0cb368fed3141c92536637f9d17a9
NoeBerdoz <berdoznoe@gmail.com>
2024-10-29 18:45:28 +0100

    Let's connect to the database

---
+++ src/main/java/ch/nb/persistence/DatabaseConnection.java
@@ -0,0 +1,41 @@
+package ch.nb.persistence;
+
+import ch.nb.logging.SimpleLogger;
+import ch.nb.utils.PropertiesLoader;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DatabaseConnection {
+    private static Connection connection;
+
+    private static PropertiesLoader propertiesLoader = new PropertiesLoader("database.properties");
+
+    private static final String DB_URL = propertiesLoader.getProperty("db.url");
+    private static final String DB_USER = propertiesLoader.getProperty("db.user");
+    private static final String DB_PASSWORD = propertiesLoader.getProperty("db.password");
+
+    public static Connection getConnection() {
+        if (connection == null) {
+            // Create a new connection
+            try {
+                connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+                SimpleLogger.info("Connection established!");
+            } catch (Exception e) {
+                SimpleLogger.error("Failed to connect to database");
+                throw new RuntimeException("Failed to connect to database", e);            }
+        }
+        return connection;
+    }
+
+    public static void closeConnection() {
+        if (connection != null) {
+            try {
+                connection.close();
+                connection = null;
+            } catch (Exception e) {
+                throw new RuntimeException("Failed to close database connection", e);
+            }
+        }
+    }
+}
