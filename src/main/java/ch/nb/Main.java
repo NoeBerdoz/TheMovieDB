bf0c2acb749440ab79a1cc03bf452b96ccdaf520
NoeBerdoz <berdoznoe@gmail.com>
2024-10-29 18:46:12 +0100

    I had fun creating my own little rookie logger

--- src/main/java/ch/nb/Main.java
+++ src/main/java/ch/nb/Main.java
@@ -1,5 +1,7 @@
 package ch.nb;
 
+import ch.nb.logging.SimpleLogger;
+import ch.nb.persistence.DatabaseConnection;
 import ch.nb.service.ApiService;
 import ch.nb.service.MovieService;
 import com.github.cliftonlabs.json_simple.JsonException;
@@ -18,15 +20,17 @@ public class Main {
         //ApiService.searchMovies("The Matrix");
 
         // GET A MOVIE DETAILS BY ID
-        ApiService.getMovieDetails("624860");
-        ApiService.getMovieDetails("624861");
-        ApiService.getMovieDetails("624860");
-        ApiService.getMovieDetails("624859");
-        ApiService.getMovieDetails("62560");
+//        ApiService.getMovieDetails("624860");
+//        ApiService.getMovieDetails("624861");
+//        ApiService.getMovieDetails("624860");
+//        ApiService.getMovieDetails("624859");
+//        ApiService.getMovieDetails("62560");
+//
+//        MovieService movieService = MovieService.getInstance();
+//        movieService.printMovie();
+//
+//        ApiService.getMovieCredits("624860");
 
-        MovieService movieService = MovieService.getInstance();
-        movieService.printMovie();
-
-        ApiService.getMovieCredits("624860");
+        DatabaseConnection.getConnection();
     }
 }
