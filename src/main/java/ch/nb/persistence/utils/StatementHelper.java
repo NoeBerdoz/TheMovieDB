8d5a25ddc8fda10288f15a17753f9eaca4f29ec5
NoeBerdoz <berdoznoe@gmail.com>
2024-11-18 22:37:38 +0100

    Add more bind manageable cases

--- src/main/java/ch/nb/persistence/utils/StatementHelper.java
+++ src/main/java/ch/nb/persistence/utils/StatementHelper.java
@@ -2,30 +2,39 @@ package ch.nb.persistence.utils;
 
 import ch.nb.logging.SimpleLogger;
 
+import java.math.BigDecimal;
 import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.time.LocalDate;
-
+/**
+ * Helper class for binding parameters to SQL prepared statements.
+ * This class handles various data types and their specific binding to SQL queries.
+ */
 public class StatementHelper {
 
-    // Bind parameters to a prepared statement.
+    /**
+     * Binds the given parameters to a prepared statement.
+     * This method dynamically binds multiple parameters of various types
+     * to a prepared statement in the correct format.
+     *
+     * @param statement the prepared statement to bind parameters to
+     * @param sqlParameters the parameters to bind, in the order they appear in the SQL query
+     */
     public static void bindStatementParameters(PreparedStatement statement, Object... sqlParameters) {
         for (int i = 0; i < sqlParameters.length; i++) {
             Object parameter = sqlParameters[i];
             try {
-                if (parameter instanceof Integer) {
-                    statement.setInt(i + 1, (Integer) parameter);
-                } else if (parameter instanceof String) {
+                if (parameter instanceof String) {
                     statement.setString(i + 1, (String) parameter);
-                } else if (parameter instanceof Float) {
-                    statement.setFloat(i + 1, (Float) parameter);
+                } else if (parameter instanceof Long) {
+                    statement.setLong(i + 1, (Long) parameter);
+                } else if (parameter instanceof BigDecimal) {
+                    statement.setBigDecimal(i + 1, (BigDecimal) parameter);
                 } else if (parameter instanceof LocalDate) {
-                    statement.setDate(i + 1, (Date) parameter);
+                    statement.setDate(i + 1, Date.valueOf((LocalDate) parameter));
                     // TODO Work in progress
-                }
-
-                else {
+                } else {
                     statement.setObject(i + 1, parameter);
                 }
             } catch (SQLException e) {
